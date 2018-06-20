37d718575a54c22465eaf04dab4c7dae838c1461
William Van Woensel <william.van.woensel@gmail.com>
2018-06-20 12:47:00 -0300

    added OWL2 RL axioms, comments

--- src/main/java/OWL2RL.java
+++ src/main/java/OWL2RL.java
@@ -15,16 +15,24 @@ public class OWL2RL {
 		ClassLoader clsLoader = OWL2RL.class.getClassLoader();
 
 		Model model = ModelFactory.createDefaultModel();
+
+		// load accompanying axioms
 		model.read(clsLoader.getResourceAsStream("axioms.nt"), "", "N-TRIPLE");
+		// load ontology + dataset
 		model.read(clsLoader.getResourceAsStream("pizza.owl"), "", "N3");
 
+		// load & parse rules
 		Path path = Paths.get(clsLoader.getResource("owl2rl.jena").toURI());
 		String owl2rl = new String(Files.readAllBytes(path));
 		List<Rule> rules = Rule.parseRules(owl2rl);
 
+		// create inf model
 		GenericRuleReasoner reasoner = new GenericRuleReasoner(rules);
 		InfModel infModel = ModelFactory.createInfModel(reasoner, model);
 
+		// get deductions
+		// (e.g., DominosMargheritaPizza will have inferred types CheesyPizza,
+		// ItalianPizza)
 		Model deductions = infModel.getDeductionsModel();
 		deductions.write(System.out, "N3");
 	}
